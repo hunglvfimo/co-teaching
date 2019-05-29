96538b0170d1d328e75949c836840b22ec596321
hunglv <hunglv@fimo.edu.vn>
2019-05-29 13:53:12 +0900

    add support RestNet 18,34 pre-trained

--- models.py
+++ models.py
@@ -2,36 +2,50 @@ import os
 
 import torch
 import torch.nn as nn
-import torchvision.models as models
+import torch.utils.model_zoo as model_zoo
 
 from networks import ResNet, CoTeachingCNN
-from layers import Bottleneck
+from layers import Bottleneck, BasicBlock
 from contanst import *
 
-def load_model(backbone, n_classes, return_embedding, pt_model_name=None, pt_n_classes=-1):
-	# default is to create brand-new model from scratch
+model_urls = {
+	'ResNet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+	'ResNet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+	'ResNet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+	'ResNet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+	'ResNet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+}
+
+def load_model(backbone, t_n_classes, return_embedding, pt_model_name=None, pt_n_classes=-1):
+	if pt_model_name is None:
+		n_classes = t_n_classes
+	else:
+		n_classes = pt_n_classes
+
+	# create a place-holder model
 	if backbone == "ResNet50":
 		model = ResNet(Bottleneck, [3, 4, 6, 3], num_classes=n_classes, return_embedding=return_embedding)
+	elif backbone == "ResNet34":
+		model = ResNet(BasicBlock, [3, 4, 6, 3], num_classes=n_classes, return_embedding=return_embedding)
+	elif backbone == "ResNet18":
+		model = ResNet(BasicBlock, [2, 2, 2, 2], num_classes=n_classes, return_embedding=return_embedding)
 	else:
 		# default model
 		model = CoTeachingCNN(num_classes=n_classes, return_embedding=return_embedding)
 	
-	if pt_model_name is not None: # load a pre-trained model
-		if pt_model_name == "ImageNet": # load a pre-trained model from torchvision
-			if backbone == "ResNet50":
-				model = models.resnet50(pretrained=True)
-			# TODO: add more support model
+	if pt_model_name is not None :
+		# load a pre-trained model from torch
+		if pt_model_name == "ImageNet": 
+			if backbone in model_urls.keys():
+				model.load_state_dict(model_zoo.load_url(model_urls[backbone]))
+			else:
+				print("No pre-trained model for selected backbone on ImageNet")
+				pass
 		else:
 			# load a pre-trained model from local storage
 			pt_model_path = os.path.join(MODEL_DIR, pt_model_name)
 			if os.path.isfile(pt_model_path):
 				# yay it exist!
-				# we need to initialize a place-holder model with same number of classes as pre-trained one
-				if backbone == "ResNet50":
-					model = ResNet(Bottleneck, [3, 4, 6, 3], num_classes=pt_n_classes, return_embedding=return_embedding)
-				else:
-					model = CoTeachingCNN(num_classes=pt_n_classes, return_embedding=return_embedding)
-				
 				_, file_extension = os.path.splitext(pt_model_name)
 				if file_extension == '.pth':
 					checkpoint = torch.load(pt_model_path)
@@ -48,7 +62,7 @@ def load_model(backbone, n_classes, return_embedding, pt_model_name=None, pt_n_c
 					model_state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
 				
 				model.load_state_dict(model_state_dict)
-		
+	
 		# change last FC layer to output our number of classes
 		if n_classes != pt_n_classes:
 			num_ftrs = model.fc.in_features
